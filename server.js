ddeefdc5630c5c1d73bb231ee84cf784102cbfe9
mrdkprj <longsight.d.m@gmail.com>
2023-08-07 17:17:01 +0900

    fix ua

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ const createHeaders = (referer, session) => {
     headers["x-requested-with"] = "XMLHttpRequest";
     headers["x-csrftoken"] = session.csrfToken;
     if (session.userAgent) {
-        headers["user-agent"] = session.userAgent;
+        headers["user-agent"] = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"; //session.userAgent
     }
     return headers;
 };
